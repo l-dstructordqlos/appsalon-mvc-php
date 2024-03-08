91aeaa9b1b7501be171fcc2cdadb1cc7e4171f4a
l-dstructordqlos <158864464+l-dstructordqlos@users.noreply.github.com>
2024-03-08 08:39:23 -0500

    Update app.php

--- includes/app.php
+++ includes/app.php
@@ -3,10 +3,11 @@
 use Model\ActiveRecord;
 require __DIR__ . '/../vendor/autoload.php';
 
-require 'funciones.php';
-require 'database.php';
 $dotenv = Dotenv\Dotenv::createInmutable(__DIR__);
 $dotenv->safeLoad();
+require 'funciones.php';
+require 'database.php';
+
 
 // Conectarnos a la base de datos
 ActiveRecord::setDB($db);
